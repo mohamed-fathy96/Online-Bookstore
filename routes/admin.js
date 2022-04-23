628d1bbdb00eb1d87efa3d3c2efe76721c777488
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-04-23 22:57:47 +0200

    adding views, products controller, navigation, and styling

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const productController = require('../controllers/productController');
+
+// /admin/add-product => GET
+
+router.get('/add-product', productController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productController.postAddProduct);
+
+module.exports = router;
+
